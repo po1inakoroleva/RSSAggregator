f657aac7428d4f0b67778d95f2f20e99274d6958
polinakoroleva <polinakorolevaaaaa@gmail.com>
2023-08-30 15:16:47 +0700

    add "source-map-loader"

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
         test: /\.(scss)$/,
         use: [
           {
